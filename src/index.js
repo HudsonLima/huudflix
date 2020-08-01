17e0a688243c15758c7d0a8b108adf3dbb88818b
Hudson Lima <hudsonlima.ti@gmail.com>
2020-08-01 20:57:03 -0300

    Adding router functionality

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './pages/Home';
+import AddMovie from './pages/register/movie';
+import AddCategory from './pages/register/category';
+import {  BrowserRouter, Switch, Route} from 'react-router-dom';
 import './index.css';
-import App from './App';
+
+const Error404Page = () => (<div><h1>Error 404 Not Found</h1></div>);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+  <Switch>
+    <Route path="/" component={Home} exact />
+    <Route path="/register/movie" component={AddMovie} />
+    <Route path="/register/category" component={AddCategory} />
+    <Route component={Error404Page} />
+  </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
